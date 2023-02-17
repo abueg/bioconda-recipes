07680208001dea6c2fbb9882f7563b33f5c85378
Linelle Abueg <48773001+abueg@users.noreply.github.com>
2023-02-17 16:14:21 -0500

    export statements

--- recipes/hifiasm/build.sh
+++ recipes/hifiasm/build.sh
@@ -3,5 +3,9 @@
 mkdir -p $PREFIX/bin
 
 #make INCLUDES="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -lz -lpthread -lm" CC=${CC} CXX=${CXX}
-make CXXFLAGS="-g -I$PREFIX/include -O3 -msse4.2 -mpopcnt -fomit-frame-pointer -Wall" LDFLAGS="-L$PREFIX/lib -lz -lpthread -lm" CC=${CC} CXX=${CXX}
+#make CXXFLAGS="-g -I$PREFIX/include -O3 -msse4.2 -mpopcnt -fomit-frame-pointer -Wall" LDFLAGS="-L$PREFIX/lib -lz -lpthread -lm" CC=${CC} CXX=${CXX}
+export CXXFLAGS="-g -I$PREFIX/include -O3 -msse4.2 -mpopcnt -fomit-frame-pointer -Wall"
+export LDFLAGS="-L$PREFIX/lib"
+
+make CC=${CC} CXX=${CXX} ${CXXFLAGS} ${LDFLAGS} -lz -lpthread -lm
 cp hifiasm $PREFIX/bin
